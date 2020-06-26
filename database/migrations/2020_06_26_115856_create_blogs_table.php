293f6ef726526786ba00ddb443d9ed0eda333027
RYANWILLIAMS <r007will@gmail.com>
2020-06-26 15:14:40 +0300

    Blog migration without relationship

---
+++ database/migrations/2020_06_26_115856_create_blogs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->bigIncrements('blog_id');
+            $table->string('blog_title');
+            $table->longText('blog_body');
+            //add user foreign key later
+            
+            $table->timestamps('blog_created');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
