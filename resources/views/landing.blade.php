10304b29d6e3319c64c2d1cb47bff70fe71029bd
Omi1 <anthonyangatia@gmail.com>
2020-07-10 11:54:51 +0300

    Updated Jats NavBar

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -89,31 +89,30 @@
       </div>
 
       <!-- Footer  -->
-      <div class="container-fluid mt-4 px-5">
-        <div class="row">
-          <div class="col-4">
-            <h6> <a href="https://www.facebook.com/">Facebook</a> </h6>
-            <h6> <a href="https://twitter.com/home">Twitter</a> </h6>
-            <h6> <a href="https://www.linkedin.com/feed/">LinkedIn</a> </h6>
-            <h6> <a href="https://www.instagram.com/">Instagram</a> </h6>
-          </div>
-          <div class="col-4">
-            <h6> Email us for any inquirees you have about us </h6>
-            <h6> info@asperatravels.co.ke </h6>
-            <h6> Contact us </h6>
-            <h6> 0712345678 </h6>
-          </div>
-          <div class="col-4">
-            <form>
-              <div class="form-group">
-                <label for="exampleInputEmail1">Subscribe here</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-              </div>
-            </form>
-          </div>
+      <!-- <div class="row">
+        <div class="col-4">
+          <h6> <a href="https://www.facebook.com/">Facebook</a> </h6>
+          <h6> <a href="https://twitter.com/home">Twitter</a> </h6>
+          <h6> <a href="https://www.linkedin.com/feed/">LinkedIn</a> </h6>
+          <h6> <a href="https://www.instagram.com/">Instagram</a> </h6>
         </div>
-        @include('inc.footer')
+        <div class="col-4">
+          <h6> Email us for any inquirees you have about us </h6>
+          <h6> info@asperatravels.co.ke </h6>
+          <h6> Contact us </h6>
+          <h6> 0712345678 </h6>
+        </div>
+        <div class="col-4">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Subscribe here</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+          </form>
+        </div>
+      </div> -->
+      @include('inc.footer')
 </body>
 
 </html>
